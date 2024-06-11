671be12ef35d0645549a2a7e47a2b2815c0a6bd7
emumnik <nickmumladze@gmail.com>
2024-06-11 18:09:25 +0400

    main.js

--- main.js
+++ main.js
@@ -1,61 +1,37 @@
-function calculateArray(n) {
-    if (n <= 2) {
-        return 'Invalid input. Please enter a number greater than 2.';
+def expo(base, exponent, callback):
+    if exponent == 0:
+        return callback(1)
+    elif exponent == 1:
+        return callback(base)
+    else:
+        return expo(base, exponent - 1, lambda x: callback(base * x))
+
+result = expo(5, 3, lambda x: x)
+print(result) 
+
+
+
+function deepCopyAsync(obj) {
+    return new Promise((resolve, reject) => {
+        if (obj === null || typeof obj !== 'object' || Array.isArray(obj)) {
+            reject(new Error('Argument is not a valid object'));
+            return;
+        }
+        try {
+            const copiedObject = JSON.parse(JSON.stringify(obj));
+            resolve(copiedObject);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+(async () => {
+    try {
+        const original = { a: 1, b: { c: 2 } };
+        const copied = await deepCopyAsync(original);
+        console.log('Original:', original);
+        console.log('Copied:', copied);
+    } catch (error) {
+        console.error('Error:', error.message);
     }
-
-    const sumFirstSecond = 1 + 2;
-
-    const numbers = Array.from({ length: n - 2 }, (_, i) => i + 3);
-
-    const productFromThird = numbers.reduce((product, number) => product * number, 1);
-
-    return [sumFirstSecond, productFromThird];
-}
-
-const result = calculateArray(5);
-console.log(result);
-
-
-function getCity(user) {
-    return user?.banks?.[2]?.address?.city;
-}
-
-const user1 = {
-    banks: [
-        { address: { city: 'City1' } },
-        { address: { city: 'City2' } },
-        { address: { city: 'City3' } }
-    ]
-};
-
-const user2 = {
-    banks: [
-        { address: { city: 'City1' } },
-        { address: { city: 'City2' } }
-    ]
-};
-
-console.log(getCity(user1));
-console.log(getCity(user2));
-
-
-function deepClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-}
-
-const original = {
-    name: 'John',
-    age: 30,
-    address: {
-        city: 'New York',
-        zip: '10001'
-    },
-    hobbies: ['reading', 'traveling']
-};
-
-const cloned = deepClone(original);
-
-console.log(cloned);
-console.log(cloned === original);
-console.log(cloned.address === original.address);
-console.log(cloned.hobbies === original.hobbies);
+})();
